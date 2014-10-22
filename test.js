b3089a751b937abb7f8b4c849ddb35ecfe2a23ab
Frank Roth <frankred@web.de>
2014-10-22 22:11:53 +0200

    Click example added

--- test.js
+++ test.js
@@ -3,3 +3,6 @@ var mouse = require('./build/Release/mouzy');
 // Move cursor 100 pixel to the right and 200 pixel down
 // mouse.move(deltaX, deltaY);
 mouse.move(100, -100);
+
+// Mouse click
+mouse.click();
